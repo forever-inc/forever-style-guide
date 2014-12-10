fdfff0c086ff120f079aa5ea91c47a8173df9ac8
nmcclay <Nikkio101@hotmail.com>
2014-12-10 15:01:17 -0500

    Add voice and tone partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ForeverStyleGuide::Engine.routes.draw do
     get "/personas", to: "pages#personas"
+    get "/voice_and_tone", to: "pages#voice_and_tone"
     mount StyleGuide::Engine => "/", :as => :style unless Rails.env.production?
 end
