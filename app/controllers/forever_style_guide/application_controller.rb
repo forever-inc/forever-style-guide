9ab3727f93dd92d4cbf12aad1fd3a270cc340070
nmcclay <Nikkio101@hotmail.com>
2014-12-10 14:14:39 -0500

    Updated core application layout to be used consistently between style-guide and custom views

--- app/controllers/forever_style_guide/application_controller.rb
+++ app/controllers/forever_style_guide/application_controller.rb
@@ -1,4 +1,5 @@
 module ForeverStyleGuide
   class ApplicationController < ActionController::Base
+    layout "style_guide/application"
   end
 end
--- app/controllers/forever_style_guide/pages_controller.rb
+++ app/controllers/forever_style_guide/pages_controller.rb
@@ -1,6 +1,9 @@
 module ForeverStyleGuide
   class PagesController < ForeverStyleGuide::ApplicationController
     def personas
+      @sections = StyleGuide::Engine.config.style_guide.sections
+      @current_section = @sections.detect { |section| section.title == "Personas" }
+      render "forever_style_guide/personas/show"
     end
   end
 end
