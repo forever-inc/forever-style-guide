9137d83bbeca5530b7e05cfe05d682cd8586de53
nmcclay <Nikkio101@hotmail.com>
2014-12-11 00:06:51 -0500

    Updated sophia persona layout

--- lib/forever_style_guide/engine.rb
+++ lib/forever_style_guide/engine.rb
@@ -4,6 +4,6 @@ module ForeverStyleGuide
   class Engine < ::Rails::Engine
     isolate_namespace ForeverStyleGuide
     config.style_guide.paths << self.root.join("app/views/forever_style_guide/**/*")
-    config.assets.debug = true
+    # config.assets.debug = true
   end
 end
