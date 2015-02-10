338409555b5f9fe015ecd995aca7f97b6b9d8035
nmcclay <Nikkio101@hotmail.com>
2015-02-10 16:58:52 -0500

    Re-enabled UX Guide and Voice and Tone section

--- app/controllers/forever_style_guide/style_controller.rb
+++ app/controllers/forever_style_guide/style_controller.rb
@@ -6,16 +6,16 @@ module ForeverStyleGuide
       @current_section = @sections.detect { |section| section.id == params[:id] }
     end
 
-    # def ux_guide
-    #   @sections = StyleGuide::Engine.config.style_guide.sections
-    #   @current_section = @sections.detect { |section| section.title == "Ux Guide" }
-    #   render "forever_style_guide/ux_guide/show"
-    # end
-    #
-    # def voice_and_tone
-    #   @sections = StyleGuide::Engine.config.style_guide.sections
-    #   @current_section = @sections.detect { |section| section.title == "Voice And Tone" }
-    #   render "forever_style_guide/voice_and_tone/show"
-    # end
+    def ux_guide
+      @sections = StyleGuide::Engine.config.style_guide.sections
+      @current_section = @sections.detect { |section| section.title == "Ux Guide" }
+      render "forever_style_guide/content/ux_guide/show"
+    end
+
+    def voice_and_tone
+      @sections = StyleGuide::Engine.config.style_guide.sections
+      @current_section = @sections.detect { |section| section.title == "Voice And Tone" }
+      render "forever_style_guide/content/voice_and_tone/show"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ForeverStyleGuide::Engine.routes.draw do
-  get "/:id" => "style#show", :as => :style
   root to: "style#index"
+  get "/ux_guide", to: "style#ux_guide"
+  get "/voice_and_tone", to: "style#voice_and_tone"
+  get "/:id" => "style#show", :as => :style
 end
