4dcadfa1f0a6c8e9fe66948e3076d21f5c0b3a6b
nmcclay <Nikkio101@hotmail.com>
2015-02-17 16:02:21 -0500

    Updated index page to actually look like something interesting to land on

--- app/helpers/forever_style_guide/application_helper.rb
+++ app/helpers/forever_style_guide/application_helper.rb
@@ -1,5 +1,10 @@
 module ForeverStyleGuide
   module ApplicationHelper
+
+    def style_guide_version
+      VERSION
+    end
+
     def escape_for_display(content)
       content.gsub(/\</, "&lt;").gsub(/\>/, "&gt;").html_safe
     end
