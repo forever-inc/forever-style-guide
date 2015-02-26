5f9b8ce14557e625cf25d2d26797678842b1d328
nmcclay <Nikkio101@hotmail.com>
2015-02-26 11:24:56 -0500

    Added optional icon to section headers

--- lib/forever_style_guide/config.rb
+++ lib/forever_style_guide/config.rb
@@ -3,6 +3,10 @@ require "style_guide/config"
 module ForeverStyleGuide
   class Config < StyleGuide::Config
 
+    def style_guide_decorators
+      @style_guide_decorators ||= YAML.load(File.open(ForeverStyleGuide::Engine.root.join("config","style_guide.yml"), 'r'))['style_guide']
+    end
+
     def self.sections_path
       File.join("forever_style_guide", "sections")
     end
--- lib/forever_style_guide/section.rb
+++ lib/forever_style_guide/section.rb
@@ -4,6 +4,10 @@ require "forever_style_guide/partial"
 module ForeverStyleGuide
   class Section < StyleGuide::Section
 
+    def icon
+      @icon ||= ForeverStyleGuide::Engine.config.style_guide.style_guide_decorators['sections'][@id]['icon']
+    end
+
     def partials
       partial_paths.map { |path| ForeverStyleGuide::Partial.new(path, self) }.sort_by { |p| p.title }
     end
