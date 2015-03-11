9abbe3f364ba883f5e0a9cb7880efdd987a490cf
nmcclay <Nikkio101@hotmail.com>
2015-03-11 19:36:48 -0400

    Added feature_bullet component and refactored css in both style_guide and forever_style_guide to be more modular for this component.

--- app/helpers/forever_style_guide/application_helper.rb
+++ app/helpers/forever_style_guide/application_helper.rb
@@ -3,6 +3,10 @@ require 'pathname'
 module ForeverStyleGuide
   module ApplicationHelper
 
+    def style_guide_root
+      ForeverStyleGuide::Engine.root
+    end
+
     def style_guide_version
       VERSION
     end
@@ -14,5 +18,11 @@ module ForeverStyleGuide
     def escape_for_display(content)
       content.gsub(/\</, "&lt;").gsub(/\>/, "&gt;").html_safe
     end
+
+    def escape_erb_for_display(partial_path)
+      partial = File.read("#{style_guide_root}/app/views/#{partial_path}")
+      escape_for_display(partial)
+    end
+
   end
 end
