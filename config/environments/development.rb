2b7df613f122728b764311acfd1b10a57effcf64
nmcclay <Nikkio101@hotmail.com>
2015-05-07 11:55:23 -0400

    moving live reload dependency out to dummy for compatibility reasons

--- config/environments/development.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.configure do
-  unless ENV['DISABLE_LIVERELOAD']
-    # Inject livereload.js into the head
-    config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
-  end
-end
--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -34,4 +34,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  unless ENV['DISABLE_LIVERELOAD']
+    # Inject livereload.js into the head
+    config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
+  end
 end
