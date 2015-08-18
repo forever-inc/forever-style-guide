024e16106f0e373091b079389b0e05ad41de847d
nmcclay <Nikkio101@hotmail.com>
2015-08-18 16:14:50 -0400

    Added animated transition effect to dropdown

--- app/assets/javascripts/forever_style_guide/navbar.js
+++ app/assets/javascripts/forever_style_guide/navbar.js
@@ -4,6 +4,27 @@ $(function () {
   $(document).on('click', '.navbar-dropdown .dropdown-menu', function(e) {
     e.stopPropagation()
   });
+
+  //$('.dropdown').on('mouseover', function(e) {
+  //  var $this = $(this);
+  //  $this.trigger('show.bs.dropdown');
+  //});
+  //
+  //$('.dropdown').on('mouseout', function(e) {
+  //  //var $this = $(this);
+  //  //$this.trigger('hide.bs.dropdown');
+  //});
+  //
+  //// ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  //$('.dropdown').on('show.bs.dropdown', function (e) {
+  //  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  //});
+  //
+  //// ADD SLIDEUP ANIMATION TO DROPDOWN //
+  //$('.dropdown').on('hide.bs.dropdown', function (e) {
+  //  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  //});
+
 });
 
 
