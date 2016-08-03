4480e5fc997acdf915a54af8447b6e0dbf18e0cc
Don Lettrich <dlettrich@forever.com>
2016-08-03 14:23:06 -0400

    fixed up js to work with turbolinks, check the checkboxes and trigger submit on enclosing form if it has data-listToggle set to true (making that functionality optional in the markup)

--- app/assets/javascripts/forever_style_guide/list-toggle.js
+++ app/assets/javascripts/forever_style_guide/list-toggle.js
@@ -1,19 +1,26 @@
-var setupListToggle = function () {
-  $(".list-group-toggle-trigger").click(function (e) {
-    e.preventDefault();
-    $(this).next('.list-group-item-child').toggleClass("is_open");
-    $("i", this).toggleClass("fa-plus fa-minus");
-  });
+var setup_toggle_list = function(){
+    $(".list-group-toggler").click(function (e) {
+        e.preventDefault();
+        $(this).next('.list-group-item-child').toggleClass("is_open");
+        $("i", this).toggleClass("fa-plus fa-minus");
+    });
 
-  var activeListItems = $('.list-group-link.is_active');
+    $('.list-group-link').on('click', function(e) {
+        e.preventDefault();
+        var checkBox = $(this).next("input[type='checkbox']");
+        checkBox.prop("checked", !checkBox.prop("checked"));
+        $(this).parents('form[data-listToggle]').submit();
+    });
 
-  if ($(activeListItems).length) {
-    $(activeListItems).append('<i class="fa fa-times"></i>');
-    $(activeListItems).parents('.list-group-item-child').addClass('is_open');
-    $(activeListItems).parents('.list-group-item').addClass('is_active');
-    $(activeListItems).parents('.list-group-item').find('.list-group-toggle-trigger .fa').toggleClass("fa-plus fa-minus");
-  }
+    var activeListItems = $('.list-group-link.is_active');
+
+    if ($(activeListItems).length) {
+        $(activeListItems).append('<i class="fa fa-times"></i>');
+        $(activeListItems).next("input[type='checkbox']").prop('checked', true);
+        $(activeListItems).parents('.list-group-item-child').addClass('is_open');
+        $(activeListItems).parents('.list-group-item').addClass('is_active');
+        $(activeListItems).parents('.list-group-item').find('.list-group-toggle-trigger .fa').toggleClass("fa-plus fa-minus");
+    }
 };
-
-$(document).ready(setupListToggle);
-$(document).on('page:load', setupListToggle);
+$(document).ready(setup_toggle_list);
+$(document).on('page:load', setup_toggle_list);
