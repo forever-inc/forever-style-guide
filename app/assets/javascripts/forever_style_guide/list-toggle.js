168b9640787e72d5dcdad6353b32d20beed8251e
Don Lettrich <don.lettrich@gmail.com>
2016-08-02 20:11:53 -0400

    fix for turbolinks page:load vs document.ready in list-toggle script, also fixes css selector error for activeListItems

--- app/assets/javascripts/forever_style_guide/list-toggle.js
+++ app/assets/javascripts/forever_style_guide/list-toggle.js
@@ -1,13 +1,11 @@
-$(function () {
+var setupListToggle = function () {
   $(".list-group-toggle-trigger").click(function (e) {
     e.preventDefault();
     $(this).next('.list-group-item-child').toggleClass("is_open");
     $("i", this).toggleClass("fa-plus fa-minus");
-  })
-});
+  });
 
-$(function () {
-  var activeListItems = $('.list-group-link-is_active');
+  var activeListItems = $('.list-group-link.is_active');
 
   if ($(activeListItems).length) {
     $(activeListItems).append('<i class="fa fa-times"></i>');
@@ -15,4 +13,7 @@ $(function () {
     $(activeListItems).parents('.list-group-item').addClass('is_active');
     $(activeListItems).parents('.list-group-item').find('.list-group-toggle-trigger .fa').toggleClass("fa-plus fa-minus");
   }
-});
+};
+
+$(document).ready(setupListToggle);
+$(document).on('page:load', setupListToggle);
