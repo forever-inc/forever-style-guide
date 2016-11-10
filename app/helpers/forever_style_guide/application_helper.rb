1f5b96ed5dbf653fd90f6e2161b71f828a3b1cb4
Robert Berger <rberger@forever.com>
2016-11-10 11:28:51 -0500

    Allow ambassadors to specifty full URL's. (#126)
    * Allow ambassadors to specifty full URL's.
    
    Allows URL's in ambassador records to include http: and https: protocols.
    
    Also handles partial URL's that begin with the host name.
    
    When testing for presence of a URL in an amabassador record, use present? to ignore blank values.
    
    * Use absolute URL's in title attribute of link tags.
    
    * Move absolute_url helper to a more appropriate location in account_helpers.rb

--- app/helpers/forever_style_guide/application_helper.rb
+++ app/helpers/forever_style_guide/application_helper.rb
@@ -32,6 +32,16 @@ module ForeverStyleGuide
       url = URI.decode(url.to_s)
     end
 
+    def absolute_url(url_str)
+      return  unless url_str.present?
+
+      url_str = url_str.strip
+
+      return url_str if url_str =~ /https?\:/i
+
+      "http://" + url_str
+    end
+
     def has_item_in_cart?
       defined?(current_order) && current_order.product_count > 0
     end
