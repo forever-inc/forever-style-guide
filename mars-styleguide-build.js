891d075861a80a7da4f92bd91859345ce16c64c3
Zach Schweitzer <zschweitzer@forever.com>
2017-02-10 13:27:17 -0500

    Dev to Master 3.1.2 (#150)
    * makes manifest imports for project Mars
    
    * Modifications to mars-manifests, pull in bs-sass and fa from npm, comment out last error.
    
    * Remove leftover  @debug
    
    * First stab at node script to compile assets for mars
    
    * Actually commit script
    
    * Completed first draft of script, add logging, npm script to run postinstall, srcmap output
    
    * fiddling
    
    * fiddling
    
    * build script working on npm install from mars-ui, uncomment scss line that was problematic to webpack but works with node-sass.
    
    * capture one last folder structure possibility for resolving node modules.
    
    * Remove problematic postinstall (on mac) and have consumer run script. Add node_modules to gitignore.
    
    * adds iin all the new categories to the nav
    
    * begins to style new nav sections
    
    * begins to adjust spacing details
    
    * finishes styling new nav
    
    * bumps version
    
    * begins addressing PR comments
    
    * uses new font weight vars
    
    * Add maars checkbox implementation styles for checked and disabled states.
    
    * refines spacing in nav and cleans up code smells
    
    * continues to make design and code improvements for nav
    
    * chages css to work in IE
    
    * mobile nav style tweeks
    
    * clean up nav icon css
    
    * changes promotions url to be deals
    
    * makes mobile cart and cart-count overlap in design
    
    * adds new nav modules to mars manifest
    
    * add in print photos link
    
    * adjust spacing of nav dropdown links
    
    * adjust nav alignment
    
    * fixes mobile nav not being able to scroll
    
    * Remove deleted references from Mars manifests, add modals, comment out sample image that kills webpack
    
    * Add mars manifest build task to package
    
    * removes form-controll styles that arent being used
    
    this is because all pages have bootstrap being loaded in before and
    after and so bootstrap overrides these styles
    
    * moves demo specific modal code into its own css file
    
    * Fix broken mars build (#146)
    
    * Fix broken mars build
    
    * Testing
    
    * Remove unneeded file.
    
    * Remove comments
    
    * Feature/modal xl/137988163 (#147)
    
    * adds modal xl
    
    * widens xl modal
    
    * Bug/modal vars (#148)
    
    * adds vars for modal xl
    
    * typo fix
    
    * bumps to 3.1.2 - updates primary nav breakpoints to delay hamburger … (#149)
    
    * bumps to 3.1.2 - updates priumary nav breakpoints to delay hamburger until 960px
    
    * correct display of account popover dividers
    
    * correct display of help popover dividers
    
    * accounts for a scenario where a logged in user with no items in cart overflows the nav
    
    * adds mixin for multi-line-ellipsization and implements for long user names in account popover
    
    * address cart count positioning
    
    * adjust dropdown spacer shim for My Forever nav
    
    * fix back office link display

---
+++ mars-styleguide-build.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const fs = require('fs');
+const cp = require('child_process');
+const sass = require('node-sass');
+const copy = require('copy');
+
+class MarsStyleGuideBuild {
+  
+  constructor() {
+    this.outputFolder = path.resolve(__dirname, './dist');
+    this.srcFolder = path.resolve(__dirname, './app/assets/');
+    
+  }
+
+  log(...args) {
+    console.log(' -', ...args);
+  }
+  
+  cleanDist() {
+    this.log('clean: attempt removal of dist folder');
+    cp.execSync(`rm -rf ${this.outputFolder}`);
+    this.log('clean: removed dist folder');
+    return this;
+  }
+
+  copyAssets() {
+    this.log('copy: copying forever styleguide fonts');
+    copy(path.join(this.srcFolder, 'fonts/**/*'), path.join(this.outputFolder, 'fonts'), function () {});
+
+    this.log('copy: copying forever styleguide images');
+    copy(path.join(this.srcFolder, 'images/**/*'), path.join(this.outputFolder, 'images'), function () {});
+
+    this.log('copy: copying font-awesome fonts');
+    copy(path.join(this.resolveNodeModuleFolder('font-awesome'), 'fonts/**/*'), path.join(this.outputFolder, 'fonts'), function () {});
+    
+    return this;
+  }
+
+  sassBuild(entryFile) {
+    const outputFile = path.resolve(__dirname, this.outputFolder, 'css/styleguide.css');
+    
+    const options = {
+      file: entryFile,
+      outFile: outputFile,
+      sourceMap: true,
+      outputStyle: 'compressed',
+      importer: (url, prev, done) => {
+        
+        //replace bootstrap imports
+        if (url.startsWith('bootstrap')) {
+          done({
+            file: url.replace('bootstrap/', `${this.resolveNodeModuleFolder('bootstrap-sass')}/assets/stylesheets/bootstrap/`)
+          });
+        } 
+        
+        //replace font-awesome imports
+        else if (url.startsWith('font-awesome')) {
+          done({
+            file: url.replace('font-awesome', `${this.resolveNodeModuleFolder('font-awesome')}/scss/font-awesome`)
+          });
+        } 
+        
+        //replace forever style guide imports
+        else if (url.startsWith('forever_style_guide')) {
+          done({
+            file: url.replace('forever_style_guide/', `${__dirname}/app/assets/stylesheets/forever_style_guide/`)
+          });
+        } 
+        
+        //no replace
+        else {
+          done();
+        }
+      }
+    };
+
+    this.log('sass: starting render');
+    const result = sass.render(options, (err, result) => {
+      if (!err) {
+        cp.execSync(`mkdir -p ${path.resolve(__dirname, this.outputFolder, 'css')}`);
+        fs.writeFileSync(outputFile, result.css);
+        fs.writeFileSync(path.resolve(__dirname, this.outputFolder, 'css/styleguide.css.map'), result.map);
+        this.log('sass: css rendered successfully');
+      } else {
+        this.log('sass: something went wrong in render step', err);
+        process.exit(1);
+      }
+    });
+
+    return this;
+  }
+
+  resolveNodeModuleFolder(moduleName) {
+    const parent = path.resolve(process.cwd(), '../', moduleName);
+    const child = `${__dirname}/node_modules/${moduleName}`;
+    const other = `${process.cwd()}/node_modules/${moduleName}`;
+    if (fs.existsSync(parent)) {
+      return parent;
+    } else if (fs.existsSync(child)) {
+      return child;
+    } else if (other) {
+      return other;
+    }
+    console.error(`ERROR: Module not found: ${moduleName}.`);
+    console.error(`  - Ensure you have run 'npm install'.`);
+    console.error(`  - This script must be run from root of either the styleguide or a parent project folder.`);
+    process.exit(1);
+  }
+  
+}
+
+console.log('BEGIN: Mars Styleguide Build');
+new MarsStyleGuideBuild()
+  .cleanDist()
+  .copyAssets()
+  .sassBuild(`${__dirname}/app/assets/stylesheets/style_guide/application-mars.scss`);
