20e1b0411f1be7e9b4b9d9f0b02a2cad00e74f1f
Aminata Ballo <aminataballo@Animata-Ballos-MacBook-Pro.local>
2017-10-18 10:34:21 -0400

    update trademarks to use upcase and tm symbols correctly

--- app/helpers/forever_style_guide/application_helper.rb
+++ app/helpers/forever_style_guide/application_helper.rb
@@ -5,12 +5,16 @@ module ForeverStyleGuide
 
     def trademark(copy)
       case copy
-      when "Forever", "Forever Historian", "Forever Valet", "pixels2Pages", "Historian", "Forever Live!"
+      when "Forever"
+        copy.upcase
+      when "Forever", "Forever Historian", "Forever Valet", "Forever Live!", "Forever Print"
+        copy[0..6].upcase + copy[7..copy.length-1] + "™"
+      when "pixels2Pages", "Historian"
         copy += "™"
-      when "Forever Account", "Forever Guarantee", "Forever Guarantee Fund", "Forever Membership", "Forever Guarantee Fund", "Forever Members", "Forever Retreats", "Forever Ambassador", "Forever Ambassadors", "Forever Print"
-        copy[0..6] + "™" + copy[7..copy.length-1]
+      when "Forever Account", "Forever Guarantee", "Forever Guarantee Fund", "Forever Membership", "Forever Guarantee Fund", "Forever Members", "Forever Retreats", "Forever Ambassador", "Forever Ambassadors"
+        copy[0..6].upcase + "™" + copy[7..copy.length-1]
       when "Forever Artisan", "Artisan", "Forever Storage"
-        copy += "®"
+          copy[0..6].upcase + copy[7..copy.length-1] += "®"
       else
         copy
       end
