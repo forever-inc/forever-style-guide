4dec86462298e6ee3960825704750a3ec95067eb
Bradley Fox <psu.bfox@gmail.com>
2018-01-17 13:44:54 -0500

    Uses rails engine

--- lib/forever_style_guide.rb
+++ lib/forever_style_guide.rb
@@ -1,5 +1,6 @@
-require "style-guide"
+#require "style-guide"
 require "forever_style_guide/engine"
+#require_relative '../app/helpers/forever_style_guide/application_helper'
 
 module ForeverStyleGuide
 end
