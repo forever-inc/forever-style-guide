6dba04078206f2fa08ad1f2c03beca9d96b40d2c
Bradley Fox <psu.bfox@gmail.com>
2018-03-05 16:29:54 -0500

    Chore/upgrade rails 5.1/3837376 (#244)
    * epic/rails-5-upgrade/3837376
    
    * Uses rails engine
    
    * Clean up
    
    * chore/rails-5.1-upgrade/3837376
    
    * Removed controller
    
    * Upped ruby version
    
    * Upped gem version

--- lib/forever_style_guide/engine.rb
+++ lib/forever_style_guide/engine.rb
@@ -1,8 +1,4 @@
-require "style-guide"
 require "forever_style_guide/version"
-require "forever_style_guide/config"
-require "forever_style_guide/section"
-require "forever_style_guide/partial"
 
 require 'sass-rails'
 require 'jquery-rails'
@@ -13,6 +9,6 @@ module ForeverStyleGuide
   class Engine < ::Rails::Engine
     isolate_namespace ForeverStyleGuide
     engine_name 'forever_style_guide'
-    config.style_guide = ForeverStyleGuide::Config.new
+
   end
 end
