d475f1c068126885ab192f096dbf667c6131f325
Tom Reid <treid@forever.com>
2021-09-01 15:40:40 -0400

    86 purge and jot properties from base tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  //mode: 'jit',
-  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
