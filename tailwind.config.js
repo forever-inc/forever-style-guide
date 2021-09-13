d479ebc2e712594e8291912a34fa4dcbf1a00ab7
Tom Reid <treid@forever.com>
2021-09-13 14:07:10 -0400

    Remove larger sizes in tw config for now to clean up dev size

--- tailwind.config.js
+++ tailwind.config.js
@@ -149,8 +149,8 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px',
-      '3xl': '1750px'
+      //'2xl': '1536px',
+      //'3xl': '1750px'
     },
 
     fontWeight: {
